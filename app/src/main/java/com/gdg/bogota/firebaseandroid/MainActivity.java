03ee3ee57f0dbbd2990ffc3a707e3d1fbc17f33f
Santiago Carrillo <sancarbar@gmail.com>
2016-05-26 15:44:16 -0500

    Added anonymous authentication.

--- app/src/main/java/com/gdg/bogota/firebaseandroid/MainActivity.java
+++ app/src/main/java/com/gdg/bogota/firebaseandroid/MainActivity.java
@@ -1,37 +1,52 @@
 package com.gdg.bogota.firebaseandroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity
     extends AppCompatActivity
+    implements FirebaseAuth.AuthStateListener
 {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+
+    @Bind( R.id.welcome_message )
+    TextView welcomeMessage;
+
+    @Bind( R.id.login_button )
+    View loginButton;
+
+    @Bind( R.id.logout_button )
+    View logoutButton;
+
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        ButterKnife.bind( this );
         Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab );
-        fab.setOnClickListener( new View.OnClickListener()
-        {
-            @Override
-            public void onClick( View view )
-            {
-                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG ).setAction( "Action",
-                                                                                                       null ).show();
-            }
-        } );
     }
+
 
     @Override
     public boolean onCreateOptionsMenu( Menu menu )
@@ -56,5 +71,55 @@ public class MainActivity
         }
 
         return super.onOptionsItemSelected( item );
+    }
+
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        firebaseAuth.addAuthStateListener( this );
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener( this );
+    }
+
+
+    @Override
+    public void onAuthStateChanged( @NonNull FirebaseAuth firebaseAuth )
+    {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if ( user != null )
+        {
+            welcomeMessage.setVisibility( View.VISIBLE );
+            logoutButton.setVisibility( View.VISIBLE );
+            loginButton.setVisibility( View.GONE );
+            Log.d( TAG, "onAuthStateChanged:signed_in:" + user.getUid() );
+        }
+        else
+        {
+            welcomeMessage.setVisibility( View.GONE );
+            logoutButton.setVisibility( View.GONE );
+            loginButton.setVisibility( View.VISIBLE );
+            Log.d( TAG, "onAuthStateChanged:signed_out" );
+        }
+    }
+
+    public void onLoginClicked( View view )
+    {
+        firebaseAuth.signInAnonymously();
+        loginButton.setEnabled( false );
+    }
+
+    public void onLogoutClicked( View view )
+    {
+        firebaseAuth.signOut();
+        logoutButton.setVisibility( View.GONE );
+        loginButton.setEnabled( true );
+        loginButton.setVisibility( View.VISIBLE );
     }
 }
